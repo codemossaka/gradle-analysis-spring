58f8074e6f4940d48f19020d6bfadb9f5f201433
Essonga Godson <peyagodson@ymail.com>
2022-12-07 17:35:13 +0300

    first commit

---
+++ src/main/java/com/example/demomurtuz/DemoMurtuzApplication.java
@@ -0,0 +1,13 @@
+package com.example.demomurtuz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMurtuzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMurtuzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demomurtuz/DemoMurtuzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demomurtuz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMurtuzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
